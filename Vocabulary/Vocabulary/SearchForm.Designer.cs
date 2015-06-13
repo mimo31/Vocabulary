d31a5691de6ac8db389212e774ed856d4256a96a
mimo31 <weweuuuu@email.cz>
2015-06-13 09:47:14 +0200

    Redesigned SearchForm

--- Vocabulary/Vocabulary/SearchForm.Designer.cs
+++ Vocabulary/Vocabulary/SearchForm.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.lookForPages = new System.Windows.Forms.CheckBox();
+            this.lookForWords = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -46,25 +47,29 @@
             this.label1.Text = "Search";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // checkBox1
+            // lookForPages
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 98);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(56, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Pages";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.lookForPages.AutoSize = true;
+            this.lookForPages.Checked = true;
+            this.lookForPages.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.lookForPages.Location = new System.Drawing.Point(19, 98);
+            this.lookForPages.Name = "lookForPages";
+            this.lookForPages.Size = new System.Drawing.Size(56, 17);
+            this.lookForPages.TabIndex = 1;
+            this.lookForPages.Text = "Pages";
+            this.lookForPages.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // lookForWords
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(19, 121);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(57, 17);
-            this.checkBox2.TabIndex = 2;
-            this.checkBox2.Text = "Words";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.lookForWords.AutoSize = true;
+            this.lookForWords.Checked = true;
+            this.lookForWords.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.lookForWords.Location = new System.Drawing.Point(19, 121);
+            this.lookForWords.Name = "lookForWords";
+            this.lookForWords.Size = new System.Drawing.Size(57, 17);
+            this.lookForWords.TabIndex = 2;
+            this.lookForWords.Text = "Words";
+            this.lookForWords.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -77,30 +82,40 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 144);
+            this.textBox1.Location = new System.Drawing.Point(12, 508);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(435, 20);
+            this.textBox1.Size = new System.Drawing.Size(533, 20);
             this.textBox1.TabIndex = 4;
             // 
-            // button1
+            // label3
             // 
-            this.button1.Location = new System.Drawing.Point(453, 144);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Search results";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 182);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(533, 316);
+            this.listBox1.TabIndex = 6;
             // 
             // SearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 177);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(557, 540);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lookForWords);
+            this.Controls.Add(this.lookForPages);
             this.Controls.Add(this.label1);
             this.Name = "SearchForm";
             this.Text = "SearchForm";
@@ -112,10 +127,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox lookForPages;
+        private System.Windows.Forms.CheckBox lookForWords;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Vocabulary/Vocabulary/SearchForm.cs
+++ Vocabulary/Vocabulary/SearchForm.cs
@@ -16,5 +16,6 @@ namespace Vocabulary
         {
             InitializeComponent();
         }
+
     }
 }
